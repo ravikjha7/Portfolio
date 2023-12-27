45bd916f99cbc65b6c09e7228a8fcdc6971f18b2
ravikjha7 <ravikjha7@gmail.com>
2023-12-27 12:13:47 +0530

    Homepage changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Portfolio/tree/gh-pages/",
+  base:"https://github.com/ravikjha7/Portfolio/tree/gh-pages/",
   plugins: [react()],
  
 })
